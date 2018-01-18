ba33052baf97d83e758a17e13e2e2dac1c9d96d4
DESKTOP-B1DT327\Shivani Tiwary <shivanit.mcs17.du@gmail.com>
2018-01-17 22:22:43 -0800

    Number is even or odd?

---
+++ EvenOdd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{ 
+	int a;
+	cout<<"Enter a number\t";
+	cin>>a;
+ 
+	if(a%2==0)
+		cout<<"\n Number is even";
+	else 
+		cout<<"\n Number is odd\n";
+}
