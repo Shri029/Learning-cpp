ed33f6eecb5ac668faec164f0f143225f3414ef7
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2018-01-26 04:10:40 -0800

    Single Linked List

--- Slliist.cpp
+++ Slliist.cpp
@@ -1,3 +1,4 @@
+//Single Linked List
 #include <iostream>
 using namespace std;
 
@@ -18,21 +19,25 @@ template <class T>
 class sllist{
     
     node<T> *head,*tail;
-    int c;
     
     public:
     sllist(){
         
         head=tail=0;
-        c=-1;
     }
     
     int isempty();
     void addtohead(T);
     void addtotail(T);
+    void insert_in_middle(int, T);
+    int isinlist(T);
     T deletefromhead();
     T deletefromtail();
+    void deletenode(T);
+    void deleteinode(int);
     sllist<T> reverse();
+    void reverse_one_pass();
+    sllist<T> operator+(sllist);
     void display();
 };
 
@@ -148,10 +153,131 @@ sllist<T> sllist<T>::reverse(){
     return o1;
 }     
 
+template <class T>
+void sllist<T>:: deletenode(T x){
+    
+    if(!isempty()){
+        if(head==tail && head->info==x){
+            delete head;
+            head=tail=0;
+        }
+        else if(head->info==x)
+            deletefromhead();
+        else{
+            node<T> *prev,*cur;
+            prev=head;
+            cur=prev->next;
+            
+            while(cur!=0 && cur->info!=x){
+                prev=cur;
+                cur=cur->next;
+            }
+            
+            if(cur!=0){
+                prev->next=cur->next;
+                if(tail==cur)
+                 tail=prev;
+                delete cur;
+            }
+            else
+             cout<<"\nElement not found";
+        }
+    }
+    else
+     cout<<"\nList is empty";
+}
+
+template <class T>
+void sllist<T>:: deleteinode(int x){
+   int i=1; 
+   node<T> *temp=head;
+   
+   while(temp!=0 && i!=x){
+       i++;
+       temp=temp->next;
+   }
+    if(i==x && temp!=0)
+     deletenode(temp->info);
+    else
+     cout<<"\nElement does not exist";
+}    
+
+template <class T>
+void sllist<T>:: insert_in_middle(int i,T x){
+    
+    int k=1;
+    node<T> *temp=new node<T>(x);
+    node<T> *prev=head;
+    
+    while(k!=(i-1) && prev!=0){
+        k++;
+        prev=prev->next;
+    }
+    
+    if(prev!=0 && k==(i-1)){
+        if(tail==prev){
+            tail->next=temp;
+            tail=temp;
+        }
+        else{
+            temp->next=prev->next;
+            prev->next=temp;
+        }
+    }
+    else
+     cout<<"\nPosition not found";
+}
+
+template <class T>
+sllist<T> sllist<T>:: operator+(sllist<T> o1){
+    
+    node<T> *temp=o1.head;
+    
+    while(temp!=0){
+        
+        addtotail(temp->info);
+        temp=temp->next;
+    }
+    return (*this);
+}
+
+template <class T>
+void sllist<T>:: reverse_one_pass(){
+    
+    node<T> *temp1[10];
+    node<T> *temp=head;
+    int i,c=-1;
+    
+    while(temp!=0){
+        temp1[++c]=temp;
+        temp=temp->next;
+    }
+    
+    for(i=c;i>0;i--)
+        temp1[i]->next=temp1[i-1];
+    
+    temp1[i]->next=0;
+    head=temp1[c];
+}
+
+template <class T>
+int sllist<T>:: isinlist(T x)
+{
+    node<T> *temp=head;
+    
+    while(temp!=0){
+        if(temp->info==x)
+         return 1;
+        else 
+         temp=temp->next;
+    }
+    return 0;
+}
+
 int main() {
     
     sllist<int> l1,l2;
-    int a,ch,e;
+    int a,ch,e,i;
     char c;
     
     do{
@@ -207,6 +333,50 @@ int main() {
                 l2.display();
                 break;
                 
+        case 6: cout<<"\nEnter the element you want to search:";
+                cin>>a;
+                
+                e=l1.isinlist(a);
+                
+                if(e==1)
+                 cout<<"\nElement found";
+                else
+                 cout<<"\nElement not found";
+                break; 
+        
+        case 7: cout<<"\nEnter the element you want to delete:";
+                cin>>a;
+                l1.deletenode(a);
+                break;
+                
+        case 8: cout<<"\nEnter the index of element you want to delete:";
+                cin>>a;
+                
+                l1.deleteinode(a);
+                break;
+        
+        case 9: cout<<"\nEnter the element and index where you want to insert:";
+                cin>>a>>i;
+                
+                l1.insert_in_middle(i,a);
+                break;
+                
+        case 10: cout<<"\nInsert in list 2";
+                do{
+                 cout<<"\nEnter the value:";
+                 cin>>a;
+                 l2.addtotail(a);
+                 
+                 cout<<"\nWant to insert more?";
+                 cin>>c;
+                }while(c=='y');
+                
+                l1=l1.operator+(l2);
+                break;
+                
+        case 11:
+                l1.reverse_one_pass();
+                break;        
         }        
        cout<<"\nWant to continue?";
        cin>>c; 
