5f26d7f6f13d8b5b185ce9366ace68d6b86dfc6e
DESKTOP-B1DT327\Shivani Tiwary <shivanit.mcs17.du@gmail.com>
2018-02-19 22:39:04 +0530

    Pyramid

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,10 +1,11 @@
 //Program to print pyramid using *
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-	int i,j;
+	int i,j,n=5;
 	char ch='A';
 
 //--------------------------Half Pyramids--------------------------	
@@ -37,6 +38,24 @@ int main() {
 	    cout<<endl;
 	}
 	cout<<"\n\n\n"; 
+
+    for(i=1;i<=5;i++){                                 //Pyramid
+    
+        cout<<setw((n+1)-i);
+	    for(j=1;j<=(2*i-1);j++)
+	    cout<<"*";
+	    cout<<endl;
+	}
+	cout<<"\n\n\n";	
 	
+	for(i=1;i<=5;i++){                                 //Pyramid
+    
+        cout<<setw((n+1)-i);
+	    for(j=1;j<=(2*i-1);j++)
+	    cout<<j;
+	    cout<<endl;
+	}
+	cout<<"\n\n\n";	
+   	
 	return 0;
 }
