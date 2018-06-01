13cb80359b3b064f0e2d0f7d3e82053e6395d80c
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2018-06-01 22:41:21 +0530

    Update GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -3,6 +3,14 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int a,int b){  //Recursive Method
+    
+   	if(b==0)
+     	 return a;
+    	else 
+      	 return gcd(b,a%b);
+}
+
 int main()
 {
 	int x,y,r;
@@ -11,13 +19,13 @@ int main()
 	cout<<"\nEnter another number:";
 	cin>>y;
 
-    r=x%y;
-	while(r!=0)
-	{	x=y;
-     	y=r;
-		r=x%y;
+   	r=x%y;
+	while(r!=0){
+	  x=y;
+     	  y=r;
+	  r=x%y;
 	}
 	cout<<"\nGcd of no is:"<<y;
-	
+	cout<<"\nGcd of no is:"<<gcd(x,y);  //Recursive
 	return 0;
 }
