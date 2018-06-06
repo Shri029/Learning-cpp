c9e7f31bbbe7b083af042f19bd9a2b6f0fa6ec4e
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2018-06-06 17:28:05 +0530

    STL(set)

---
+++ K_SmallestLargestSTL.cpp
@@ -0,0 +1,65 @@
+// STL based C++ program to find k-th smallest element.
+
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <set>
+using namespace std;
+
+#define MAX 100
+
+void ksmallest(int a[],int n,int k){
+    
+    set<int> s;
+    int i;
+    set<int>::iterator it;
+    
+    //or  auto it=s.begin(); Initiallization and declaration 
+    
+    
+   for(i=0;i<n;i++)
+    s.insert(a[i]);
+    
+  it=s.begin();
+    
+   for (int i = 0; i < k - 1; i++)
+        it++;
+   cout<<"\nk'th smallest:"<<*it; 
+}
+
+void klargest(int a[],int n,int k){
+    
+    set<int,greater<int>> s;
+    int i;
+    set<int>::iterator it;
+    
+    //or  auto it=s.begin(); Initiallization and declaration 
+    
+    
+   for(i=0;i<n;i++)
+    s.insert(a[i]);
+    
+   it=s.begin();
+    
+   for (int i = 0; i < k-1; i++)
+        it++;
+   cout<<"\nk'th largest:"<<*it; 
+}
+
+
+int main() {
+    
+    int a[MAX];
+    int i,n,k;
+    
+    cin>>n;
+    
+    for(i=0;i<n;i++)
+    cin>>a[i];
+    
+    cin>>k;
+    
+    ksmallest(a,n,k);
+    klargest(a,n,k);
+    
+	return 0;
+}
