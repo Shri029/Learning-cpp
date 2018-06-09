d6474e9bc11e7d8a8164683baa5484657a806709
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2018-06-09 22:58:18 +0530

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+#define MAX 1000
+
 template <class T>
 class BSTnode{
 	
@@ -24,11 +26,11 @@ class BSTree{
 	BSTnode<T> *root;
 	
 	public:
-	int height;
+//	int height;
 	
 	BSTree(){
 		root=0;
-		height=-1;
+//		height=-1;
 	}
 	
     int isempty(){
@@ -46,6 +48,11 @@ class BSTree{
 	void postorder();
 	void postorder(BSTnode<T>*);
 	void breadthfirst();
+	void iterative_pre();
+	void iterative_post();
+	void iterative_in();
+	void height();
+	int height(BSTnode<T>*);
 };
 
 //-----------------------------Insertion-------------------------
@@ -74,9 +81,9 @@ void BSTree<T>::insertion(T x){
 	 prev->right=temp; 	
 }
 
-//-----------------------------Display-------------------------
+//-----------------------------TRAVERSALS-------------------------
 template <class T>
-void BSTree<T>::preorder(){
+void BSTree<T>::preorder(){                                 //Recursive Preorder 
 	preorder(root);
 }
 
@@ -89,7 +96,7 @@ void BSTree<T>::preorder(BSTnode<T> *p){
 	}
 }	
 
-template <class T>
+template <class T>                                         //Recursive Inorder 
 void BSTree<T>::inorder(){
 	inorder(root);
 }
@@ -103,7 +110,7 @@ void BSTree<T>::inorder(BSTnode<T> *p){
 	}
 }	
 
-template <class T>
+template <class T>                                         //Recursive Postorder 
 void BSTree<T>::postorder(){
 	postorder(root);
 }
@@ -111,30 +118,137 @@ void BSTree<T>::postorder(){
 template <class T>
 void BSTree<T>::postorder(BSTnode<T> *p){
 	if(p!=0){
-		preorder(p->left);
-		preorder(p->right);
+		postorder(p->left);
+		postorder(p->right);
 		cout<<p->info<<" ";
 	}
 }	
 
-template <class T>
+template <class T>                                         //BreadthFirst
 void BSTree<T>::breadthfirst(){
 	
 	queue<BSTnode<T>*> q;
-	BSTnode<T> *v=root;
+	BSTnode<T> *p=root,*v;
 	
-	q.enqueue(p);
+	q.push(p);
 	
-	while(!q.isempty()){
-		v=q.dequeue();
+	if (root == 0)
+      return;
+      
+	while(!q.empty()){
+		v=q.front();
+		q.pop();
 		
 		cout<<v->info<<" ";
 		
 		if(v->left!=0)
-		 q.enqueue(v->left);   
+		 q.push(v->left);   
 		if(v->right!=0)
-		 q.enqueue(v->right); 
+		 q.push(v->right); 
    }
+}
+
+template <class T>                                      //Iterative Preorder
+void BSTree<T>::iterative_pre(){
+	
+	stack<BSTnode<T>*> q;
+	BSTnode<T> *p=root,*v;
+	
+	q.push(p);
+	
+	if (root == 0)
+      return;
+      
+	while(!q.empty()){
+		v=q.top();
+		q.pop();
+		cout<<v->info<<" ";
+		
+		if(v->right!=0)
+		 q.push(v->right); 
+		if(v->left!=0)
+		 q.push(v->left);   
+   }
+}
+
+template <class T>                                      //Iterative Postorder
+void BSTree<T>:: iterative_post(){
+    
+    stack<BSTnode<T>*> s;
+    BSTnode<T> *p=root, *q=root;
+    
+    while(p!=0){
+        for(;p->left!=0;p=p->left)
+            s.push(p);
+        
+        while(p!=0 && (p->right==0 || p->right==q)){
+            
+            cout<<p->info<<" ";
+            q=p;
+            if(s.empty())
+             return;
+            p=s.top(); 
+            s.pop();
+        }
+        s.push(p);
+        p=p->right;
+    }
+}
+
+template <class T>                                      //Iterative Inorder
+void BSTree<T>:: iterative_in(){
+    
+    stack<BSTnode<T>*> s;
+    BSTree<T> *p=root;
+    
+    while(p!=0){
+        
+        while(p!=0){
+            if(p->right)
+              s.push(p->right);
+            s.push(p);
+            p=p->left;
+        }
+        
+        p=s.top();
+        s.pop();
+        
+        while(!s.empty() && p->right==0){
+          cout<<p->info<<" ";
+          p=s.top();
+          s.pop();
+        }
+        cout<<p->info<<" ";
+        if(!s.empty()){
+            p=s.top();
+            s.pop();
+        }
+        else
+          p=0;  
+    }
+}
+
+//--------------------------------------------------------------
+template<class T>
+void BSTree<T>::height(){
+    int h=height(root);
+    cout<<"\nHeight of tree: "<<h;
+}
+
+template<class T>
+int BSTree<T>::height(BSTnode<T> *node){
+    
+    if(node==0)
+     return 0;
+    else{
+        int hleft=height(node->left);
+        int hright=height(node->right);
+        
+        if(hleft>hright)
+            return (hleft+1);
+        else 
+            return (hright+1);
+    }
 }
 
 int main(){
@@ -142,16 +256,23 @@ int main(){
 	int x;
 	char c;
 	do{
-	cout<<"\nenter a no for tree:";
-	cin>>x;
+	cin>>x;                     //Element
 	t1.insertion(x);
-	cout<<"\ndo you want to continue:";
-	cin>>c;
+	cin>>c;                     //Continue
 	}
-	while(c=='y');
-	cout<<"\ntree is:";
 	
-	t1.breadthfirst();
+	while(c=='y');
+	
+	//----------------------Tree traversals----------------------"
+	cout<<"\nBreadthFirst: ";           t1.breadthfirst(); 
+	cout<<"\n\nRecursive Inorder: ";    t1.inorder();
+    cout<<"\nIterative Inorder: ";      t1.inorder();
+    cout<<"\n\nRecursive Preorder: ";   t1.preorder();
+	cout<<"\nIterative Preorder: ";     t1.iterative_pre();
+	cout<<"\n\nRecursive Postorder: ";  t1.postorder();
+	cout<<"\nIterative Postorder: ";    t1.iterative_post();
+	
+	t1.height();
 	
 	return 0;
 }
